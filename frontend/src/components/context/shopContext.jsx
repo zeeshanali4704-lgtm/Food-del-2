2bd9bafc44dc8491521f8fc4b26b30f178115caf
zeeshan ali <zeeshanali4704@gmail.com>
2026-07-03 15:42:25 +0500

    Update shopContext.jsx

--- frontend/src/components/context/shopContext.jsx
+++ frontend/src/components/context/shopContext.jsx
@@ -7,7 +7,7 @@ const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
-  const url = "https://food-del-2-backend-mkju.onrender.com";
+  const url = "https://food-del-2-backend-zeeshan.onrender.com";
 
   const fetchFoodList = async () => {
     try {
